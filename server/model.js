843e927ee516702031599c55298b2dd53e8fe44d
tmorin <tcmorin@gmail.com>
2013-11-16 19:10:37 +0100

    remove meteor implementation

--- server/model.js
+++
@@ -1,8 +0,0 @@
-var Projects = new Meteor.Collection("projects");
-
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-        var nbrProjects = Projects.find().count();
-        console.log('nbrProjects', nbrProjects);
-    });
-}
